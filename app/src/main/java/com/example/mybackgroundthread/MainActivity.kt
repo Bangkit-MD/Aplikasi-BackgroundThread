eb37b28ef6339c24eac8993a9b70cfbe8fcb488d
Kevin Alfito <kevinalfito69@gmail.com>
2023-03-15 21:26:37 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/mybackgroundthread/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.mybackgroundthread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val tvStatus: TextView = findViewById(R.id.tv_status)
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+        btnStart.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default) {
+//            executor.execute {
+                try {
+                    for (i in 1..10) {
+//                        Thread.sleep(500)
+                        delay(500)
+                        val precentage = i * 10
+//                        handler.post {
+
+                        withContext(Dispatchers.Main){
+                            if (precentage == 100) {
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text =
+                                    String.format(getString(R.string.compressing), precentage)
+                            }
+                        }
+//                        }
+
+
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+//        }
+
+    }
+}
